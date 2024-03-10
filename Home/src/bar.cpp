4e56b54e234803e11ff960b50649cac3d4fed830
Paras-Sachdeva <paras140902@gmail.com>
2024-03-10 23:00:35 +0530

    ADD: bar

---
+++ Home/src/bar.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<stdlib.h>
+
+
+int main()
+{
+    int gd=DETECT, gm;
+    initgraph(&gd,&gm,"C:\\MinGW\\lib\\libbgi.a");
+    setfillstyle(LINE_FILL,RED);
+    line(50,300,300,300);
+    line(50,300,50,50);
+    bar(100,150,125,300);
+    getch();
+    closegraph();
+    return 0;
+}
