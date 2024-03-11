18d34303e4564107a95f34fa09630524349cd2f1
Paras-Sachdeva <paras140902@gmail.com>
2024-03-11 22:50:44 +0530

    ADD: Concentric Circles

---
+++ Home/src/concentric.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<stdlib.h>
+
+
+int main()
+{
+    int i,j,r,c;
+    int gd=DETECT, gm;
+    initgraph(&gd,&gm,"C:\\MinGW\\lib\\libbgi.a");
+    setcolor(WHITE);
+    for(i=250,j=200,r=100,c=0;r>=5;r=r-10,c++)
+    {
+       setcolor(c);
+       circle(i,j,r);
+       setfillstyle(1,c);
+       floodfill(i,j,c);
+    }
+    getch();
+    closegraph();
+    return 0;
+}
