9c4aa3902c7f96bebe8a2126d47dd5b096dd049f
Paras-Sachdeva <paras140902@gmail.com>
2024-03-12 21:33:03 +0530

    FIX: No Need to Build First, Run Directly

--- Home/src/ellipse.cpp
+++ Home/src/ellipse.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int i, j;
     int gd=DETECT, gm;
-    initgraph(&gd,&gm,"C:\\MinGW\\lib\\libbgi.a");
+    initgraph(&gd,&gm,(char*)"");
     pieslice(getmaxx()/2,getmaxy()/2,0,180,80);
     for(i=100,j=30;j<=500;i=i+50,j=j+20)
     {
