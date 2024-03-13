b29221fe175865f66326ea3d015e6b421ef49045
Paras-Sachdeva <paras140902@gmail.com>
2024-03-13 22:35:45 +0530

    FIX: Rainbow File

--- Home/src/rainbow.cpp
+++ Home/src/rainbow.cpp
@@ -1,40 +1,28 @@
-#include <graphics.h>
-#include <math.h>
+#include<stdio.h> 
+#include<graphics.h> 
+#include<dos.h> 
 
-void drawCone(int x, int y, int radius, int height) {
-    // Draw the base circle
-    circle(x, y, radius);
+void rainbow() 
+{ 
+	int gdriver = DETECT,gmode; 
+	int x, y, i; 
 
-    // Draw lines connecting the base circle to the top point of the cone
-    line(x - radius, y, x, y - height);
-    line(x + radius, y, x, y - height);
+	initgraph(&gdriver,&gmode,"C:\\Turboc3\\BGI"); 
 
-    // Draw the lines forming the cone's lateral surface
-    float angle;
-    int x1, y1, x2, y2;
+	x = getmaxx() / 2; 
+	y = getmaxy() / 2;
 
-    for (angle = 0; angle <= 180; angle += 1) {
-        x1 = x - radius + radius * cos(angle * M_PI / 180);
-        y1 = y - height + radius * sin(angle * M_PI / 180);
-        x2 = x + radius - radius * cos(angle * M_PI / 180);
-        y2 = y - height + radius * sin(angle * M_PI / 180);
+	for (i=30; i<200; i++) 
+	{ 
+		delay(100); 
 
-        line(x1, y1, x2, y2);
-    }
-}
+		setcolor(i/10); 
 
-int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
-
-    int x = 200;    // x-coordinate of the center of the base circle
-    int y = 300;    // y-coordinate of the center of the base circle
-    int radius = 50; // radius of the base circle
-    int height = 100; // height of the cone
-
-    drawCone(x, y, radius, height);
-
-    getch();
-    closegraph();
-    return 0;
-}
+		arc(x, y, 0, 180, i-10); 
+	} 
+} 
+int main() 
+{ 
+	rainbow(); 
+	return 0; 
+} 
