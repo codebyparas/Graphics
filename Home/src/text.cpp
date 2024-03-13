fcc01b08bf24994ea8da521ac56fd4105862e6cf
Paras-Sachdeva <paras140902@gmail.com>
2024-03-13 22:36:48 +0530

    FIX: Text File

---
+++ Home/src/text.cpp
@@ -0,0 +1,30 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() 
+{
+	//initilizing graphic driver and 
+	//graphic mode variable
+	int graphicdriver=DETECT,graphicmode;
+
+	//calling initgraph with parameters
+	initgraph(&graphicdriver,&graphicmode,"c:\\turboc3\\bgi");
+
+	//Printing message for user
+	outtextxy(20, 20 + 20, "Program to print different fonts in C graphics");
+
+	//initilizing variables
+	int x = 75, y = 75, f = 0;
+
+	//for loop to print different fonts
+	for (f = 0; f <= 5; f++)
+	{
+		settextstyle(f, HORIZ_DIR, 1);
+		outtextxy(x, y, "Hello World..");
+		y = y + 20;
+	}
+
+	getch();
+
+	return 0;
+}
