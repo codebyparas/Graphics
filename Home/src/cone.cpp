69f79601a9d1275d14dcc5c23bf2a2ed5a9cc84d
Paras-Sachdeva <paras140902@gmail.com>
2024-03-17 23:55:27 +0530

    FIX: Change Cone

--- Home/src/cone.cpp
+++ Home/src/cone.cpp
@@ -5,17 +5,24 @@ int main()
 {
     int gd=DETECT, gm;
     initgraph(&gd,&gm,(char*)"");
-
-    setfillstyle(SOLID_FILL, 2);
+    
+    setcolor(RED);
     line(300, 60, 80, 320);
     line(300, 60, 520, 320);
     ellipse(300, 320, 180, 360, 220, 70);
-    floodfill(300, 61, WHITE);
+    setfillstyle(SOLID_FILL, RED);
+    floodfill(300, 61, RED);
 
-    setfillstyle(SOLID_FILL, 15);
+    setcolor(WHITE);
+    circle(300,55,30);
+    setfillstyle(SOLID_FILL, WHITE);
+    floodfill(300, 55, WHITE);
+
+    setcolor(GREEN);
     ellipse(300, 190, 180, 360, 110, 20);
     ellipse(300, 220, 180, 360, 135, 30);
-    floodfill(301, 210, WHITE);
+    // setfillstyle(SOLID_FILL, GREEN);
+    // floodfill(301, 210, GREEN);
 
     getch();
     closegraph();
