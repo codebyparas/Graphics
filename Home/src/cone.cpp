cd101e94580743d9cee7b1c547989ab9d9244047
Paras-Sachdeva <paras140902@gmail.com>
2024-03-29 23:23:57 +0530

    ADD: Green Strip to Cone

--- Home/src/cone.cpp
+++ Home/src/cone.cpp
@@ -3,28 +3,32 @@
 #include <dos.h>
 
 int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+        int gd=DETECT, gm;
+    initgraph(&gd,&gm,(char*)"");
 
-    // Draw cone
+    // Main Part of Cone
     setcolor(RED);
+    line(300, 60, 80, 320);
+    line(300, 60, 520, 320);
+    ellipse(300, 320, 180, 360, 220, 70);
     setfillstyle(SOLID_FILL, RED);
-    line(100, 200, 540, 200);
-    line(100, 200, 320, 50);
-    line(540, 200, 320, 50);
-    floodfill(320, 150, RED);
+    floodfill(300, 61, RED);
 
-    // Draw base of the cap
-    setcolor(RED);
-    setfillstyle(SOLID_FILL, RED);
-    pieslice(320, 200, 0, 180, 100);
-
-    // Draw fur
+    // White Circle on Top
     setcolor(WHITE);
+    circle(300,55,30);
     setfillstyle(SOLID_FILL, WHITE);
-    ellipse(320, 200, 0, 360, 100, 60);
-    floodfill(320, 200, WHITE);
+    floodfill(300, 55, WHITE);
 
+    // Green Strip
+    setcolor(GREEN);
+    line(190,190,165,219);
+    line(410,190,435,219);
+
+    ellipse(300, 190, 180, 360, 110, 20);
+    ellipse(300, 220, 180, 360, 135, 30);
+    setfillstyle(SOLID_FILL, GREEN);
+    floodfill(301, 210, GREEN);
     getch();
     closegraph();
     return 0;
