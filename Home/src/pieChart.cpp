92a8c4af51fc19c72d4b79d0483d5864c769047c
Paras-Sachdeva <paras140902@gmail.com>
2024-04-01 22:40:57 +0530

    EDIT: Fill Pie Chart with Different Patterns

--- Home/src/pieChart.cpp
+++ Home/src/pieChart.cpp
@@ -6,25 +6,25 @@ int main() {
    initgraph(&gd, &gm, "C:\\TC\\BGI");
  
    settextstyle(BOLD_FONT,HORIZ_DIR,2);
-   outtextxy(220,10,"PIE CHART");
+   outtextxy(220,30,"PIE CHART");
    /* Setting cordinate of center of circle */
    x = getmaxx()/2;
    y = getmaxy()/2;
  
    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,1);
-   setfillstyle(SOLID_FILL, RED);
+   setfillstyle(LINE_FILL, RED);
    pieslice(x, y, 0, 60, 120);
    outtextxy(x + 140, y - 70, "FOOD");
  
-   setfillstyle(SOLID_FILL, YELLOW);
+   setfillstyle(HATCH_FILL, YELLOW);
    pieslice(x, y, 60, 160, 120);
    outtextxy(x - 30, y - 170, "RENT");
  
-   setfillstyle(SOLID_FILL, GREEN);
+   setfillstyle(WIDE_DOT_FILL, GREEN);
    pieslice(x, y, 160, 220, 120);
    outtextxy(x - 250, y, "ELECTRICITY");
  
-   setfillstyle(SOLID_FILL, BROWN);
+   setfillstyle(INTERLEAVE_FILL, BROWN);
    pieslice(x, y, 220, 360, 120);
    outtextxy(x, y + 150, "SAVINGS");
  
